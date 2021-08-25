006d1af6410d5da5689d078000c7358568c2048c
afshaiqbal <afshaiqbal43@gmail.com>
2021-08-25 19:13:52 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import {React,useState} from 'react';
 import './App.css';
 
+
 function App() {
+ const[text1,setText1]=useState("");
+ const[text2,setText2]=useState("");
+   const change=()=>{
+     var temp=text2;
+   setText2(text1);
+   setText1(temp);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="input1">
+      <input type ="text" placeholder="text" value={text1} onChange={event => setText1(event.target.value)}
+>
+      </input>
+      <button className="button" onClick={change}>swap</button>
     </div>
+    <div className="input2">
+      <input type="text" placeholder="text" value={text2} onChange={event => setText2(event.target.value)}>
+      </input>
+      <button className="button" onClick={change}>swap</button>
+    </div>
+    </div>
+    </>
   );
 }
 
